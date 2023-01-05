e5bbde180cfbf2140207bed672defc9a748c442e
saviogodinho2002 <saviogmoiagaia.2002@gmail.com>
2023-01-05 16:30:01 -0300

    back-add: iniciado

--- back/controle-caixa/src/main/kotlin/com/api/controlecaixa/ControleCaixaApplication.kt
+++ back/controle-caixa/src/main/kotlin/com/api/controlecaixa/ControleCaixaApplication.kt
@@ -2,10 +2,19 @@ package com.api.controlecaixa
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class ControleCaixaApplication
+@RestController
+class ControleCaixaApplication{
+	@GetMapping("/")
+	fun olaMundo():String{
+		return "<div style='width:70px;height:70px;background-color: blue'> ALEXANDRE É LINDO </div>";
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ControleCaixaApplication>(*args)
+
 }
