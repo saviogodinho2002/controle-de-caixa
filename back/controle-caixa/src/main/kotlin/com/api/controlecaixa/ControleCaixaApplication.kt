28c27d13789fa41640d51c76a1b908bd2021c6b6
saviogodinho2002 <saviogmoiagaia.2002@gmail.com>
2023-01-05 16:00:06 -0300

    back-add: spring init

---
+++ back/controle-caixa/src/main/kotlin/com/api/controlecaixa/ControleCaixaApplication.kt
@@ -0,0 +1,11 @@
+package com.api.controlecaixa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControleCaixaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControleCaixaApplication>(*args)
+}
---
+++ back/controle-caixa/src/test/kotlin/com/api/controlecaixa/ControleCaixaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.controlecaixa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControleCaixaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
